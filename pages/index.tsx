275ecdb54115d4f43c20f425f99ee2c6419e257d
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-21 13:13:40 +0200

    testing deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,46 +26,47 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
-    <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
-      <Head>
-        <title>Leonardo - Portfolio</title>
-      </Head>
+    // <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
+    //   <Head>
+    //     <title>Leonardo - Portfolio</title>
+    //   </Head>
 
-      <Header socials={socials} />
+    //   <Header socials={socials} />
 
-      <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo} />
-      </section>
+    //   <section id="hero" className="snap-start">
+    //     <Hero pageInfo={pageInfo} />
+    //   </section>
 
-      <section id="about" className="snap-center">
-        <About pageInfo={pageInfo} />
-      </section>
+    //   <section id="about" className="snap-center">
+    //     <About pageInfo={pageInfo} />
+    //   </section>
 
-      <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences} />
-      </section>
+    //   <section id="experience" className="snap-center">
+    //     <WorkExperience experiences={experiences} />
+    //   </section>
 
-      <section id="skills" className="snap-start">
-        <Skills skills={skills} />
-      </section>
+    //   <section id="skills" className="snap-start">
+    //     <Skills skills={skills} />
+    //   </section>
 
-      <section id="projects" className="snap-start">
-        <Projects projects={projects} />
-      </section>
+    //   <section id="projects" className="snap-start">
+    //     <Projects projects={projects} />
+    //   </section>
 
-      <section id="contact" className="snap-start">
-        <ContactMe />
-      </section>
+    //   <section id="contact" className="snap-start">
+    //     <ContactMe />
+    //   </section>
 
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex flex-col items-center justify-center">
-            <ChevronDoubleUpIcon className="h-10 w-10 bg-gray-800 animate-bounce rounded-full p-2 border border-white/50 filter sepia hover:sepia-0 cursor-pointer" />
-            <p className="text-xs pt-2 text-gray-400">Back to top</p>
-          </div>
-        </footer>
-      </Link>
-    </div>
+    //   <Link href="#hero">
+    //     <footer className="sticky bottom-5 w-full cursor-pointer">
+    //       <div className="flex flex-col items-center justify-center">
+    //         <ChevronDoubleUpIcon className="h-10 w-10 bg-gray-800 animate-bounce rounded-full p-2 border border-white/50 filter sepia hover:sepia-0 cursor-pointer" />
+    //         <p className="text-xs pt-2 text-gray-400">Back to top</p>
+    //       </div>
+    //     </footer>
+    //   </Link>
+    // </div>
+    <h1>Hello</h1>
   );
 };
 
@@ -86,5 +87,6 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       projects,
       socials,
     },
+    revalidate: 10,
   };
 };
