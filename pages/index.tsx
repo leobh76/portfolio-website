c563ceae522339caf893b31ad6138e5b93082495
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-21 13:17:17 +0200

    debugging deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,67 +26,66 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
-    // <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
-    //   <Head>
-    //     <title>Leonardo - Portfolio</title>
-    //   </Head>
+    <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
+      <Head>
+        <title>Leonardo - Portfolio</title>
+      </Head>
 
-    //   <Header socials={socials} />
+      <Header socials={socials} />
 
-    //   <section id="hero" className="snap-start">
-    //     <Hero pageInfo={pageInfo} />
-    //   </section>
+      <section id="hero" className="snap-start">
+        <Hero pageInfo={pageInfo} />
+      </section>
 
-    //   <section id="about" className="snap-center">
-    //     <About pageInfo={pageInfo} />
-    //   </section>
+      <section id="about" className="snap-center">
+        <About pageInfo={pageInfo} />
+      </section>
 
-    //   <section id="experience" className="snap-center">
-    //     <WorkExperience experiences={experiences} />
-    //   </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience experiences={experiences} />
+      </section>
 
-    //   <section id="skills" className="snap-start">
-    //     <Skills skills={skills} />
-    //   </section>
+      <section id="skills" className="snap-start">
+        <Skills skills={skills} />
+      </section>
 
-    //   <section id="projects" className="snap-start">
-    //     <Projects projects={projects} />
-    //   </section>
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
 
-    //   <section id="contact" className="snap-start">
-    //     <ContactMe />
-    //   </section>
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
 
-    //   <Link href="#hero">
-    //     <footer className="sticky bottom-5 w-full cursor-pointer">
-    //       <div className="flex flex-col items-center justify-center">
-    //         <ChevronDoubleUpIcon className="h-10 w-10 bg-gray-800 animate-bounce rounded-full p-2 border border-white/50 filter sepia hover:sepia-0 cursor-pointer" />
-    //         <p className="text-xs pt-2 text-gray-400">Back to top</p>
-    //       </div>
-    //     </footer>
-    //   </Link>
-    // </div>
-    <h1>Hello</h1>
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex flex-col items-center justify-center">
+            <ChevronDoubleUpIcon className="h-10 w-10 bg-gray-800 animate-bounce rounded-full p-2 border border-white/50 filter sepia hover:sepia-0 cursor-pointer" />
+            <p className="text-xs pt-2 text-gray-400">Back to top</p>
+          </div>
+        </footer>
+      </Link>
+    </div>
   );
 };
 
 export default Home;
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const pageInfo: PageInfo = await fetchPageInfo();
-//   const experiences: Experience[] = await fetchExperiences();
-//   const skills: Skill[] = await fetchSkills();
-//   const projects: Project[] = await fetchProjects();
-//   const socials: Social[] = await fetchSocial();
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocial();
 
-//   return {
-//     props: {
-//       pageInfo,
-//       experiences,
-//       skills,
-//       projects,
-//       socials,
-//     },
-//     revalidate: 10,
-//   };
-// };
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  };
+};
