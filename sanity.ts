a2d50b60765a1b25037344d286159df23e4586cd
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-21 12:08:29 +0200

    trying to fix bugs on deployment

--- sanity.ts
+++ sanity.ts
@@ -1,14 +1,15 @@
 import { createClient } from "next-sanity";
-import createImgUrlBuilder from "@sanity/image-url";
+import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: "2021-03-25",
+  apiVersion: "2021-10-21",
   useCdn: process.env.NODE_ENV === "production",
 };
 
+// Set up the client for fetching data in the getProps page functions
 export const sanityClient = createClient(config);
 
 export const urlFor = (source: any) =>
-  createImgUrlBuilder(config).image(source);
+  createImageUrlBuilder(config).image(source);
