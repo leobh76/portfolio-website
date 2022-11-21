fa7493c20805ccc360eced9a0a47145bbb37e308
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-21 14:47:54 +0200

    small change for cors

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
