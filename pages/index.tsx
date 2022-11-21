37c4a20dcd855a295d71c37233d53bde42646b83
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-21 18:06:50 +0200

    debugging

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,36 +27,35 @@ type Props = {
 const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
     <div className=" bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
-      Hello World
-      {/* <Head>
+      <Head>
         <title>Leonardo - Portfolio</title>
       </Head>
 
-      <Header socials={socials} />
+      {/* <Header socials={socials} /> */}
 
-      <section id="hero" className="snap-start">
+      {/* <section id="hero" className="snap-start">
         <Hero pageInfo={pageInfo} />
-      </section>
+      </section> */}
 
-      <section id="about" className="snap-center">
+      {/* <section id="about" className="snap-center">
         <About pageInfo={pageInfo} />
-      </section>
+      </section> */}
 
-      <section id="experience" className="snap-center">
+      {/* <section id="experience" className="snap-center">
         <WorkExperience experiences={experiences} />
-      </section>
+      </section> */}
 
-      <section id="skills" className="snap-start">
+      {/* <section id="skills" className="snap-start">
         <Skills skills={skills} />
-      </section>
+      </section> */}
 
-      <section id="projects" className="snap-start">
+      {/* <section id="projects" className="snap-start">
         <Projects projects={projects} />
-      </section>
+      </section> */}
 
-      <section id="contact" className="snap-start">
+      {/* <section id="contact" className="snap-start">
         <ContactMe />
-      </section>
+      </section> */}
 
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
@@ -65,46 +64,46 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
             <p className="text-xs pt-2 text-gray-400">Back to top</p>
           </div>
         </footer>
-      </Link> */}
+      </Link>
     </div>
   );
 };
 
 export default Home;
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const pageInfo = await fetchPageInfo();
-//   const experiences = await fetchExperiences();
-//   const skills = await fetchSkills();
-//   const projects = await fetchProjects();
-//   const socials = await fetchSocial();
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocial();
+  
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  };
+};
 
-//   return {
-//     props: {
-//       pageInfo,
-//       experiences,
-//       skills,
-//       projects,
-//       socials,
-//     },
-//   };
-// };
-
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const pageInfo: PageInfo = await fetchPageInfo();
-//   const experiences: Experience[] = await fetchExperiences();
-//   const skills: Skill[] = await fetchSkills();
-//   const projects: Project[] = await fetchProjects();
-//   const socials: Social[] = await fetchSocial();
-
-//   return {
-//     props: {
-//       pageInfo,
-//       experiences,
-//       skills,
-//       projects,
-//       socials,
-//     },
-//     revalidate: 10,
-//   };
-// };
+  // export const getServerSideProps: GetServerSideProps = async () => {
+  //   const pageInfo = await fetchPageInfo();
+  //   const experiences = await fetchExperiences();
+  //   const skills = await fetchSkills();
+  //   const projects = await fetchProjects();
+  //   const socials = await fetchSocial();
+  
+  //   return {
+  //     props: {
+  //       pageInfo,
+  //       experiences,
+  //       skills,
+  //       projects,
+  //       socials,
+  //     },
+  //   };
+  // };
