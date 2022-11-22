cb5b67a27e47fceac83b029432d3f746cc93567a
Leonardo B. HUrovich <105162659+leobh76@users.noreply.github.com>
2022-11-22 14:01:24 +0200

    changes for the projects query

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,23 +26,30 @@ function Projects({ projects }: Props) {
             key={project._id}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
-            <motion.img
-              className="w-50 max-h-80 object-cover rounded-xl shadow-sm"
-              initial={{ y: -300, opacity: 0 }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              viewport={{ once: false }}
-              src={urlFor(project?.image).url()}
-              alt=""
-            />
+            <Link href={project?.linkToBuild} passHref>
+              <a target="_blank">
+                <div
+                  className="flex relative transition-all cursor-pointer 
+                  hover:after:content-['Link_to_Project'] after:opacity-0
+                  hover:after:opacity-100 after:absolute after:text-xl after:scale-50 hover:after:scale-100  after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-gray-900/50 after:shadow-black/70 after:shadow-xl after:text-[#FFE55C]/90 after:px-3 after:py-2 after:mt-[6vh]
+                   2xl:after:mt-[3vh] after:rounded-xl xs:after:text-sm sm:after:text-lg md:after:text-xl hover:after:hover:underline after:transition-all"
+                >
+                  <motion.img
+                    className="sm:h-[35vh] mt-[12vh] 2xl:h-[40vh] 2xl:mt-[6vh] sm:object-contain cursor-pointer"
+                    initial={{ y: -150, opacity: 0 }}
+                    transition={{ duration: 1.2 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    src={urlFor(project?.image).url()}
+                    alt=""
+                  />
+                </div>
+              </a>
+            </Link>
 
             <div className="space-y-6 max-w-6xl">
-              <h4 className="xs:text-lg sm:text-2xl font-semibold text-center">
-                <Link href={project?.linkToBuild} passHref>
-                  <a className="underline decoration-[#F7AB0A]/50">
-                    {project?.title}
-                  </a>
-                </Link>
+              <h4 className="xs:text-lg sm:text-2xl font-semibold text-center underline decoration-[#F7AB0A]/50">
+                {project?.title}
               </h4>
 
               <div className="flex items-center space-x-2 justify-center">
